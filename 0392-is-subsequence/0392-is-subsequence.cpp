368f4b1cf691e1284c589af5adf0bf84720d5951
Aditya Kulkarni <95539130+AdiKulkarni21@users.noreply.github.com>
2023-06-13 14:34:07 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (25.34%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         stack<char> stack;
-        for(int i=0;i<s.length();i++){
-            stack.push(s[i]);}
+        for(int i=0;i<s.length();i++)
+            stack.push(s[i]);
     
 for(int i=t.length()-1;i>=0;i--)
     if(!stack.empty() && (stack.top()==t[i]))
