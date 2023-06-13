3bd9a285868b209f87caae0e92048d40fb5e2995
Aditya Kulkarni <95539130+AdiKulkarni21@users.noreply.github.com>
2023-06-13 14:32:34 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (11.48%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        stack<char> stack;
+        for(int i=0;i<s.length();i++){
+            stack.push(s[i]);}
+    
+for(int i=t.length()-1;i>=0;i--){
+    if(!stack.empty() && (stack.top()==t[i]))
+    {
+        stack.pop();
+    }
+    
+}    
+  return stack.empty();
+    }
+};
